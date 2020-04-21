ab1d210a13919c76e0f897eb6f11216442a7a82e
san99tiago <san99tiago@gmail.com>
2020-04-21 02:58:24 -0500

    creacion archivo para maenjo de FLASK

---
+++ CURSO_08_FLASK/application.py
@@ -0,0 +1,10 @@
+#Ahora comenzaremos a trabajar con FLASK!!!!
+#Esta herramienta es un framework que nos facilita el trabajo para desarrollo de paginas web.
+#El objetivo de este es facilitar el monitoreo y manejo de "requests" y "responses"
+
+#Nota: flask NO viene en librerias por defecto, debemos instalarla adicionalmente
+#Recomiendo:  >>pip install flask   (hacer esto en terminal en carpeta de Scripts de Python a usar)
+from flask import Flask
+
+
+
