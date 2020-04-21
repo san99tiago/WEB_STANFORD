d10c104d90265f6fda2b12282e20a83344260ca6
san99tiago <san99tiago@gmail.com>
2020-04-21 03:49:37 -0500

    crear un flask dinamico con string variable segun input

---
+++ CURSO_08_FLASK/app3.py
@@ -0,0 +1,18 @@
+#CODIGO PARA MULTIPLES ROUTES EN FLASK PERSONALIZADAS SEGUN RUTA INGRESADA
+
+#Nota: flask NO viene en librerias por defecto, debemos instalarla adicionalmente (pip install flask)
+from flask import Flask
+
+#Creamos web application llamada "app" (basada en Flask), con nombre del archivo actual (__name__)
+app = Flask(__name__)
+
+#Indicamos funcion asociada a la ruta por defecto "/" (osea si NO tiene nada de ruta adicional)
+@app.route("/")
+def index():
+    return( "PRIMERA PAGINA WEB CON FLASK!!!!!")
+
+#Creamos ahora una ruta PERSONALIZADA donde se puede ingresar LO QUE SEA!!!!
+#Ahora, el servidor nos muestra un saludo, seguno lo ingresado respectivamente
+@app.route("/<string:nombre>")
+def saludar(nombre):
+    return("BUENOS DIAS, " + nombre)
