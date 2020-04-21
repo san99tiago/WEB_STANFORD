ba21162e95aa0da67d17d2af7853edd55aab776c
san99tiago <san99tiago@gmail.com>
2020-04-21 03:13:15 -0500

    archivo basico flask funcional

--- CURSO_08_FLASK/app.py
+++ CURSO_08_FLASK/app.py
@@ -6,5 +6,11 @@
 #Recomiendo:  >>pip install flask   (hacer esto en terminal en carpeta de Scripts de Python a usar)
 from flask import Flask
 
+#Creamos web application llamada "app" (basada en Flask), con nombre del archivo actual (__name__)
+app = Flask(__name__)
 
+#Indicamos funcion asociada a la ruta por defecto "/" (osea si NO tiene nada de ruta adicional)
+@app.route("/")
+def index():
+    return( "PRIMERA PAGINA WEB CON FLASK!!!!!")
 
