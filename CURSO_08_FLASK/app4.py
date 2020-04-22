fc232d441bbc5341565c88a494ee800e429f3dbc
san99tiago <san99tiago@gmail.com>
2020-04-21 23:25:10 -0500

    comenzando a conectar entre flask y html

---
+++ CURSO_08_FLASK/app4.py
@@ -0,0 +1,13 @@
+#CODIGO PARA CONECTAR HTMLs EXISTENTES A TRAVES DE FLASK
+#Ver que se conecta con "index_04.html" que se encuentra en carpeta "templates"
+
+#OJO: ahora debemos importar tanto Flask como render_template (para conectar con HTMLs)
+from flask import Flask, render_template
+
+#Creamos web application llamada "app" (basada en Flask), con nombre del archivo actual (__name__)
+app = Flask(__name__)
+
+#Indicamos funcion asociada a la ruta por defecto "/" (osea si NO tiene nada de ruta adicional)
+@app.route("/")
+def index():
+    return( render_template("index_04.html") )
