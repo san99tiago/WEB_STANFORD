c683f6fd19589b8c1f57435ed5a7ea1a29fb5ea9
san99tiago <san99tiago@gmail.com>
2020-04-26 23:24:46 -0500

    Agregamos cambio a app6.py para conexion con otro html

--- CURSO_08_FLASK/app6.py
+++ CURSO_08_FLASK/app6.py
@@ -1,6 +1,7 @@
 #CODIGO PARA CONECTAR MULTIPLES HTML A TRAVES DE FLASK
 #Ver que se conecta con "index_06_A.html" que se encuentra en carpeta "templates"
 #Tambien se conecta con "index_06_B.html" que se encuentra en carpeta "templates"
+#Lo importante es que ese segundo "index_06_B.html" se crea como "variable" (ver abajo)
 
 #Simplemente importamos datetime para crear una variable "PSEUDO-DINAMICA" que mostraremos en HTML
 import datetime
@@ -25,7 +26,8 @@ def index():
     #Generamos la correcta conexion con HMLT especificado y entregamos variable de hora
     return( render_template("index_06_A.html", VARIABLE_DESDE_FLASK=VARIABLE_DESDE_FLASK) )
 
-@app.route("/index_06_B.html")
+#Indicamos la funcion asociada a la otra ruta que podremos utilizar...
+@app.route("/index_06_B")
 def otro_index():
     #Creamos variable para indicar fecha y hora actual y enviarla al html
     VARIABLE_DESDE_FLASK = datetime.datetime.now().strftime( "%d %B %Y (%I:%M%p) " )
